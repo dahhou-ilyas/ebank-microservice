353bfc7eaa886d69228501f087744c8e8813fcd6
dahhou ilyas <dahhou.ilyas@gmail.com>
2023-10-20 21:18:02 +0100

    configure h2 database

--- src/main/java/com/sid/ebankservice/EbankServiceApplication.java
+++ src/main/java/com/sid/ebankservice/EbankServiceApplication.java
@@ -1,13 +1,37 @@
 package com.sid.ebankservice;
 
+import com.sid.ebankservice.entities.BankAccount;
+import com.sid.ebankservice.enums.AccountType;
+import com.sid.ebankservice.repositories.BankAccountRepositories;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Date;
+import java.util.UUID;
 
 @SpringBootApplication
 public class EbankServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EbankServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(BankAccountRepositories bankAccountRepositories){
+        return args -> {
+            for (int i=0;i<10;i++){
+                BankAccount bankAccount=BankAccount.builder()
+                        .id(UUID.randomUUID().toString())
+                        .type(Math.random()>0.5? AccountType.SAVING_ACCOUNT:AccountType.CURRENT_ACCOUNT)
+                        .balance(20020+Math.random()*90000)
+                        .createdAt(new Date())
+                        .currency("USD")
+                        .build();
+                bankAccountRepositories.save(bankAccount);
+            }
+        };
     }
 
 }
