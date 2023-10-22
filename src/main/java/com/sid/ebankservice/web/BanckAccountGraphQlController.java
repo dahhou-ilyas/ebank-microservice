920e9c6431cac0d2be11c4672b2fb6e34905df22
dahhou ilyas <dahhou.ilyas@gmail.com>
2023-10-22 14:59:03 +0100

    add new query

--- src/main/java/com/sid/ebankservice/web/BanckAccountGraphQlController.java
+++ src/main/java/com/sid/ebankservice/web/BanckAccountGraphQlController.java
@@ -3,6 +3,7 @@ package com.sid.ebankservice.web;
 import com.sid.ebankservice.entities.BankAccount;
 import com.sid.ebankservice.repositories.BankAccountRepositories;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -13,8 +14,14 @@ public class BanckAccountGraphQlController {
     @Autowired
     private BankAccountRepositories bankAccountRepositories;
 
+    //tout les requet de graphql est de type post
     @QueryMapping
     public List<BankAccount> accounts(){ //il faut donné le meme le noms dans le query graphql
         return bankAccountRepositories.findAll();
+    }
+    @QueryMapping
+    public BankAccount bankAccountById(@Argument String id){ //il faut donné le meme le noms dans le query graphql
+        return bankAccountRepositories.findById(id)
+                .orElseThrow(()->new RuntimeException("Account not found"));
     }
 }
